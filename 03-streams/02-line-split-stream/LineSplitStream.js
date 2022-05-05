c8538adda8e61f6c1c09d4daf4e7246f2db48897
Alexandra Larina <larinaalal@consultant.ru>
2022-05-05 18:25:40 +0300

    Adding solution for text split task

--- 03-streams/02-line-split-stream/LineSplitStream.js
+++ 03-streams/02-line-split-stream/LineSplitStream.js
@@ -4,12 +4,22 @@ const os = require('os');
 class LineSplitStream extends stream.Transform {
   constructor(options) {
     super(options);
+    this._lastLine = null;
   }
 
   _transform(chunk, encoding, callback) {
+    const lines = ((this._lastLine !== null ? this._lastLine : '') +
+      chunk.toString()).split(os.EOL);
+    this._lastLine = lines.pop();
+    for (const line of lines) {
+      this.push(line);
+    }
+    callback();
   }
 
   _flush(callback) {
+    this.push(this._lastLine !== null ? this._lastLine : '');
+    callback();
   }
 }
 
