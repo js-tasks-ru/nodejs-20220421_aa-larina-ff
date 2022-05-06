1bd60fae9e39e49c861a0b0100fc15818b4e46dc
Alexandra Larina <larinaalal@consultant.ru>
2022-05-06 22:29:17 +0300

    Adding solution for simple http server: get file api

--- 03-streams/03-file-server-get/server.js
+++ 03-streams/03-file-server-get/server.js
@@ -1,6 +1,6 @@
-const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
 
 const server = new http.Server();
 
@@ -12,7 +12,24 @@ server.on('request', (req, res) => {
 
   switch (req.method) {
     case 'GET':
+      if (pathname !== 'favicon.ico') {
+        const stream = fs.createReadStream(filepath);
+        stream.pipe(res);
 
+        stream.on('error', (error) => {
+          if (error.code === 'ENOENT') {
+            if (pathname.includes('\/')) {
+              res.writeHead(400, {'Content-Type': 'text/html'});
+              res.end('Nested path is not supported');
+            } else {
+              res.writeHead(404, {'Content-Type': 'text/html'});
+              res.end('File nod found');
+            }
+          }
+        });
+
+        stream.on('end', () => res.end());
+      }
       break;
 
     default:
