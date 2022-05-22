5bbe4a2cbfb5eb93741d9179c957783c3275c4d7
Alexandra Larina <aa.larina.ff@gmail.com>
2022-05-22 17:17:03 +0300

    Adding solution for simple http server: post file api

--- 03-streams/04-file-server-post/server.js
+++ 03-streams/04-file-server-post/server.js
@@ -1,6 +1,9 @@
+const LimitSizeStream = require('../01-limit-size-stream/LimitSizeStream');
+
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
 
 const server = new http.Server();
 
@@ -12,6 +15,49 @@ server.on('request', (req, res) => {
 
   switch (req.method) {
     case 'POST':
+      const wStream = fs.createWriteStream(filepath);
+      const limitedStream = new LimitSizeStream({limit: 1048576, encoding: 'utf-8'}); // 1 Мб
+
+      let fileExists = false;
+      fs.readdirSync(path.join(__dirname, 'files')).forEach((file) => {
+        fileExists = (file === pathname) ? true : fileExists;
+      });
+
+      if (fileExists) {
+        res.writeHead(409, {'content-type': 'text/plain'});
+        res.end('Filename with name ' + pathname + ' already exists.');
+      }
+
+      if (pathname.includes('\/')) {
+        res.writeHead(400, {'Content-Type': 'text/plain'});
+        res.end('Nested path is not supported');
+      }
+
+      req.pipe(limitedStream).pipe(wStream);
+
+      req.on('end', () => {
+        res.end();
+        wStream.end();
+      });
+
+      req.on('error', () => {
+        fs.unlink(filepath, () => {
+          console.log('Server shutdown, file was deleted');
+        });
+        limitedStream.end();
+        wStream.end();
+      });
+
+      limitedStream.on('error', (err) => {
+        res.writeHead(413, {'content-type': 'text/plain'});
+        res.end('The file size must be limited with 1 MB.');
+        wStream.end();
+      });
+
+      wStream.on('error', (err) => {
+        res.writeHead(500, {'content-type': 'text/plain'});
+        res.end('Something went wrong');
+      });
 
       break;
 
