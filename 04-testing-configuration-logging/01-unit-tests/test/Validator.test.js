08be077dcc8ccfff9eb46af550efc793356d51b2
Alexandra Larina <aa.larina.ff@gmail.com>
2022-05-24 15:22:20 +0300

    Adding test for name field type

--- 04-testing-configuration-logging/01-unit-tests/test/Validator.test.js
+++ 04-testing-configuration-logging/01-unit-tests/test/Validator.test.js
@@ -3,6 +3,21 @@ const expect = require('chai').expect;
 
 describe('testing-configuration-logging/unit-tests', () => {
   describe('Validator', () => {
+    it('валидатор проверяет тип поля name', () => {
+      const validator = new Validator({
+        name: {
+          type: 'string',
+        },
+      });
+
+      const errors = validator.validate({name: 22});
+
+      expect(errors).to.have.length(1);
+      expect(errors[0]).to.have.property('field').and.to.be.equal('name');
+      expect(errors[0]).to.have.property('error').
+          and.to.be.equal('expect string, got number');
+    });
+
     it('валидатор проверяет строковые поля', () => {
       const validator = new Validator({
         name: {
@@ -12,7 +27,7 @@ describe('testing-configuration-logging/unit-tests', () => {
         },
       });
 
-      const errors = validator.validate({ name: 'Lalala' });
+      const errors = validator.validate({name: 'Lalala'});
 
       expect(errors).to.have.length(1);
       expect(errors[0]).to.have.property('field').and.to.be.equal('name');
