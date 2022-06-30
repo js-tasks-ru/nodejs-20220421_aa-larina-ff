b6713fcf175c88cfe0ad316ebc93779998edee33
Alexandra Larina <aa.larina.ff@gmail.com>
2022-06-30 19:31:30 +0300

    Fixing getting of products by subcategory

--- 06-mongodb-mongoose/02-rest-api/controllers/products.js
+++ 06-mongodb-mongoose/02-rest-api/controllers/products.js
@@ -2,16 +2,16 @@ const Product = require('../models/Product');
 const mapProduct = require('../mappers/product');
 
 module.exports.productsBySubcategory = async function productsBySubcategory(ctx, next) {
-  const productsBySubcategory = await Product.findOne({subcategory: ctx.query.subcategory});
-  if (!productsBySubcategory) return next();
+  const {subcategory} = ctx.query;
+  if (!subcategory) return next();
 
-  const productList = await Product.find({subcategory: ctx.query.subcategory});
-
+  const productList = await Product.find({subcategory: subcategory});
   ctx.body = {products: productList.map((product) => mapProduct(product))};
 };
 
 module.exports.productList = async function productList(ctx, next) {
-  ctx.body = {products: []};
+  const productList = await Product.find();
+  ctx.body = {products: productList.map((product) => mapProduct(product))};
 };
 
 module.exports.productById = async function productById(ctx, next) {
