b6507af09ef7e062af338a5031682b10a6a5a496
Alexandra Larina <aa.larina.ff@gmail.com>
2022-07-01 21:35:52 +0300

    Adding search by title and description with weights

--- 06-mongodb-mongoose/03-search/controllers/products.js
+++ 06-mongodb-mongoose/03-search/controllers/products.js
@@ -1,3 +1,8 @@
+const Product = require('../models/Product');
+const mapProduct = require('../mappers/product');
+
 module.exports.productsByQuery = async function productsByQuery(ctx, next) {
-  ctx.body = {products: []};
+  const search = ctx.query;
+  const results = await Product.find({$text: {$search: search.query}});
+  ctx.body = {products: results.map((result) => mapProduct(result))};
 };
