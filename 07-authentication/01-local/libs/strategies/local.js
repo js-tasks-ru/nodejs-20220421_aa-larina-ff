1013e0038c105f877f9ea7ec1144e21cdbfa1002
Alexandra Larina <aa.larina.ff@gmail.com>
2022-07-04 23:44:21 +0300

    Adding local strategy for user auth

--- 07-authentication/01-local/libs/strategies/local.js
+++ 07-authentication/01-local/libs/strategies/local.js
@@ -1,8 +1,20 @@
 const LocalStrategy = require('passport-local').Strategy;
+const User = require('../../models/User');
 
 module.exports = new LocalStrategy(
     {usernameField: 'email', session: false},
-    function(email, password, done) {
-      done(null, false, 'Стратегия подключена, но еще не настроена');
+    async function(email, password, done) {
+      const user = await User.findOne({email: email});
+      if (!user) {
+        done(null, false, 'Нет такого пользователя');
+        return;
+      }
+
+      const authUser = new User(user);
+      authUser.checkPassword(password)
+          .then((pwdCorrect) => {
+            if (!pwdCorrect) done(null, false, 'Неверный пароль');
+            done(null, user);
+          });
     },
 );
